d81cfca5c88019415f6d7f3b7cd76cbb57097ed0
KFuee <jbisazac@icloud.com>
2021-11-15 11:05:45 +0100

    Configurado deploy en Heroku

--- src/index.php
+++ src/index.php
@@ -1,3 +1,9 @@
 <?php
+// Cargar el autoloader de composer
+if ($_ENV['APP_ENV'] != 'dev')
+  require_once '../vendor/autoload.php';
+else
+  require_once('/var/www/vendor/autoload.php');
+
 require "core/App.php";
 $app = new App();
