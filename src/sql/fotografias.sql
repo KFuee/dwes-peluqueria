7403ca2796da4c6af46777b8f02a218b36f35d9c
KFuee <jbisazac@icloud.com>
2021-12-12 16:56:07 +0100

    Modificado archivo sql fotografías

--- src/sql/fotografias.sql
+++ src/sql/fotografias.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `fotografias` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre_fichero` varchar(255) NOT NULL,
+  `localizacion` varchar(255) NOT NULL,
   `id_servicio` varchar(255) NOT NULL,
   `fecha_creacion` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
